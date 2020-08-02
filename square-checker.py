0dea352de66850426ac3e2fc883236e64fb352d3
James N <67208442+jamesnorbertowork@users.noreply.github.com>
2020-08-02 11:44:10 +0900

    Reviewed, updated formatting

--- square-checker.py
+++ square-checker.py
@@ -1,17 +1,21 @@
-# DETERMINE PERFECT SQUARE IF NUMBER IS POSITIVE
+#------------------------------
+# DETERMINE PERFECT SQUARE
+#------------------------------
 
 # Initialize variables
 answer = 0
 flag = False
 
 # Request an integer from the user
-x = int(input('Enter an integer: '))
+x = int(input("Enter an integer: "))
 
-# GUESS AND CHECK:
-  # 1) Switch flag to True if number is negative
-  # 2) while: answer (0) squared is less than your input, keep incrementing by one
-  # 3) if: answer squared == x, print success
-  # 4) else: print failure msg, if: negative print failure msg
+# ----- GUESS AND CHECK -----
+
+# 1) Switch flag to True if number is negative
+# 2) while: answer (0) squared is less than your input, keep incrementing by one
+# 3) if: answer squared == x, print success
+# 4) else: print failure msg, if: negative print failure msg
+
 if x < 0:
     flag = True
 while answer **2 < x:
@@ -22,3 +26,4 @@ else:
     print(x, 'is not a perfect square')
     if flag:
         print(x, 'is negative. Try', -x, 'instead ...')
+        
